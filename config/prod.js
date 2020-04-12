e9ca4e384f8ed430ed52128db4b5117917bff0a0
mdaamiransar <md.aamir.ansar@gmail.com>
2020-04-12 13:50:23 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    googleClientID: process.env.GOOGLE.CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE.SECRET_ID,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_SECRET_ID,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
